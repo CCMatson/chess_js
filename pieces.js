e127cd5f7968d3c149d35d225ec448754665f3bb
CCMatson <catherine.matson@gmail.com>
2024-04-01 11:53:35 -0700

    add pieces as png, add gameboard, set up divs for game squares, and place initial game pieces in starting places

---
+++ pieces.js
@@ -0,0 +1,8 @@
+// pieces.js
+
+const king = '<div class="piece" id="king"><img src="assets/king1.png" alt="King"></div>';
+const queen = '<div class="piece" id="queen"><img src="assets/queen1.png" alt="Queen"></div>';
+const bishop = '<div class="piece" id="bishop"><img src="assets/bishop1.png" alt="Bishop"></div>';
+const rook = '<div class="piece" id="rook"><img src="assets/rook1.png" alt="Rook"></div>';
+const knight = '<div class="piece" id="knight"><img src="assets/knight1.png" alt="Knight"></div>';
+const pawn = '<div class="piece" id="pawn"><img src="assets/pawn1.png" alt="Pawn"></div>';
