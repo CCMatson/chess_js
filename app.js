824f32b796e714d8998a37a0714cd827f5adbe75
CCMatson <catherine.matson@gmail.com>
2024-04-01 12:42:37 -0700

    add white and black pieces to starting board

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const infoDisplay = document.querySelector("info-display")
 const width = 8
 
 const startPieces = [
-rook, knight, bishop, queen, king, bishop, knight, rook,
-pawn, pawn, pawn, pawn, pawn, pawn, pawn,pawn,
+rook1, knight1, bishop1, queen1, king1, bishop1, knight1, rook1,
+pawn1, pawn1, pawn1, pawn1, pawn1, pawn1, pawn1, pawn1,
 ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ',
 ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ',
-pawn, pawn, pawn, pawn, pawn, pawn, pawn,pawn,
-rook, knight, bishop, queen, king, bishop, knight, rook,
+pawn2, pawn2, pawn2, pawn2, pawn2, pawn2, pawn2, pawn2,
+rook2, knight2, bishop2, queen2, king2, bishop2, knight2, rook2,
 ]
 
 function createBoard(){
@@ -18,7 +18,17 @@ function createBoard(){
     square.classList.add('square')
     square.innerHTML = startPiece
     square.setAttribute('square-id', i)
-    square.classList.add('black')
+    // square.classList.add('black')
+    //define the row we are in, and set the 
+    const row = Math.floor((63 - i)/8 + 1)
+    if (row % 2 === 0) {
+      square.classList.add(i % 2===0 ? 'white' : 'brown')
+    } else {
+      square.classList.add(i % 2===0 ? 'brown' : 'white')
+    }
+    // if ( i <= 15) {
+    //   square.firstChild.firstChild.classList.add('black')
+    // }
     gameBoard.append(square)
   })
 }
--- pieces.js
+++ pieces.js
@@ -1,8 +1,15 @@
 // pieces.js
 
-const king = '<div class="piece" id="king"><img src="assets/king1.png" alt="King"></div>';
-const queen = '<div class="piece" id="queen"><img src="assets/queen1.png" alt="Queen"></div>';
-const bishop = '<div class="piece" id="bishop"><img src="assets/bishop1.png" alt="Bishop"></div>';
-const rook = '<div class="piece" id="rook"><img src="assets/rook1.png" alt="Rook"></div>';
-const knight = '<div class="piece" id="knight"><img src="assets/knight1.png" alt="Knight"></div>';
-const pawn = '<div class="piece" id="pawn"><img src="assets/pawn1.png" alt="Pawn"></div>';
+const king1 = '<div class="piece" id="king1"><img src="assets/king1.png" alt="King"></div>';
+const queen1 = '<div class="piece" id="queen1"><img src="assets/queen1.png" alt="Queen"></div>';
+const bishop1 = '<div class="piece" id="bishop1"><img src="assets/bishop1.png" alt="Bishop"></div>';
+const rook1 = '<div class="piece" id="rook1"><img src="assets/rook1.png" alt="Rook"></div>';
+const knight1 = '<div class="piece" id="knight1"><img src="assets/knight1.png" alt="Knight"></div>';
+const pawn1 = '<div class="piece" id="pawn1"><img src="assets/pawn1.png" alt="Pawn"></div>';
+
+const king2 = '<div class="piece" id="king2"><img src="assets/king2.png" alt="King"></div>';
+const queen2 = '<div class="piece" id="queen2"><img src="assets/queen2.png" alt="Queen"></div>';
+const bishop2 = '<div class="piece" id="bishop2"><img src="assets/bishop2.png" alt="Bishop"></div>';
+const rook2 = '<div class="piece" id="rook2"><img src="assets/rook2.png" alt="Rook"></div>';
+const knight2 = '<div class="piece" id="knight2"><img src="assets/knight2.png" alt="Knight"></div>';
+const pawn2 = '<div class="piece" id="pawn2"><img src="assets/pawn2.png" alt="Pawn"></div>';
