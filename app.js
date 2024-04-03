94a8f7b2b4b48dee0f40906591ff9aa60b030517
CCMatson <catherine.matson@gmail.com>
2024-04-02 20:46:42 -0700

    console error the issue to fix the bug, everything works except capture piece function

--- app.js
+++ app.js
@@ -33,8 +33,9 @@ function createBoard() {
     const squareColor = (row + i) % 2 == 0 ? "white" : "brown";
     square.classList.add(squareColor);
     //if the square isn't empty, make it's first child draggable
-    
-    square.firstElementChild && square.firstElementChild.setAttribute('draggable', true)
+
+    square.firstElementChild &&
+      square.firstElementChild.setAttribute("draggable", true);
 
     // if (square.innerHTML.trim() !== "") {
     //   square.firstChild.setAttribute("draggable", true);
@@ -49,41 +50,46 @@ createBoard();
 
 const allSquares = document.querySelectorAll("#gameboard .square");
 
-console.log(allSquares, 'all squares node')
+console.log(allSquares, "all squares node");
 
 //listen for drag or drop at each square
 allSquares.forEach((square) => {
-  const piece = square.querySelector('.piece')
+  const piece = square.querySelector(".piece");
   if (piece) {
     //if there is a piece, add an event listenter
     piece.draggable = true;
-    piece.addEventListener('dragstart', (event) => {
+    piece.addEventListener("dragstart", (event) => {
       //the squareId is the number of the square where the piece started
-      const startPositionId = square.getAttribute('square-id')
-      console.log('drag started from square', startPositionId)
+      const startPositionId = square.getAttribute("square-id");
+      console.log("drag started from square", startPositionId);
       //do I need this to store the data with the drag?
-      event.dataTransfer.setData('text/plain', startPositionId)
-    })
+      event.dataTransfer.setData("text/plain", startPositionId);
+    });
   }
-  square.addEventListener('dragover', dragOver)
-square.addEventListener('drop', drop)
-
+  square.addEventListener("dragover", dragOver);
+  square.addEventListener("drop", drop);
 });
 
 function dragOver(e) {
-  e.preventDefault()
+  e.preventDefault();
 }
 
 function drop(e) {
   e.preventDefault();
   const endPositionId = parseInt(e.target.getAttribute("square-id"));
-  startPositionId = e.dataTransfer.getData('text/plain')
+  startPositionId = e.dataTransfer.getData("text/plain");
   movePiece(startPositionId, endPositionId);
 }
 
 function movePiece(start, end) {
   const startSquare = document.querySelector(`[square-id="${start}"]`);
   const endSquare = document.querySelector(`[square-id="${end}"]`);
+
+  if (!startSquare || !endSquare) {
+    console.error("start or end square wasnt found");
+    return;
+  }
+
 
   if (endSquare.innerHTML.trim() !== "") {
     console.log("caputure piece");
